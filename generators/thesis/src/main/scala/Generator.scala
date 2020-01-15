29172f522d5c6b9b4ddf4e78617c02d7c0550684
Bradley Evans <bevan006@ucr.edu>
2020-01-15 20:52:31 +0000

    Added thesis project folder.

---
+++ generators/thesis/src/main/scala/Generator.scala
@@ -0,0 +1,25 @@
+package thesis
+
+import chisel3._
+
+import freechips.rocketchip.config.{Parameters}
+import freechips.rocketchip.util.{GeneratorApp}
+import utilities.TestSuiteHelper
+
+object Generator extends GeneratorApp {
+  // add unique test suites
+  override def addTestSuites {
+    implicit val p: Parameters = params
+    TestSuiteHelper.addRocketTestSuites
+    TestSuiteHelper.addBoomTestSuites
+  }
+
+  // specify the name that the generator outputs files as
+  val longName = names.topModuleProject + "." + names.topModuleClass + "." + names.configs
+
+  // generate files
+  generateFirrtl
+  generateAnno
+  generateTestSuiteMakefrags
+  generateArtefacts
+}
